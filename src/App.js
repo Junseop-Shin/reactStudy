feae83b1198e571545e53774fa3cda0ae909a8f9
junseop_shin <nuclearbomb6518@gmail.com>
2021-12-28 20:57:28 +0900

    [study] Hooks 'useState' for functional component - use 'useState' and array destructuring assignment to assign state initial value - can change state by setter function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React from "react";
 // import MyComponent from './MyComponent';
-import Counter from './Counter';
+// import Counter from './Counter';
+import Say from "./Say";
 
 const App = () => {
-  return <Counter />;
+  return <Say />;
+  // return <Counter />;
   // return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
 };
 
