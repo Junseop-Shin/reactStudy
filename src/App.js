d5b63fb4a6669d07d94b992a5e87a8fbc2402079
junseop_shin <nuclearbomb6518@gmail.com>
2021-12-28 20:13:58 +0900

    [study] setting state for component - props is readOnly in the Component - so we can use changable member called 'state' - setState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import MyComponent from './MyComponent';
+// import MyComponent from './MyComponent';
+import Counter from './Counter';
 
 const App = () => {
-  return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+  return <Counter />;
+  // return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
 };
 
 export default App;
