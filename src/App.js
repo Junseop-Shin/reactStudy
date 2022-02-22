a3ec6d2a825b0712fdc0f476c19797c89ed13268
junseop_shin <nuclearbomb6518@gmail.com>
2022-02-22 20:49:10 +0900

    [feat] ref없이 state로 처리하는 경우 - state와 className을 이용해, 비밀번호가 맞으면 초록색 배경, 틀리면 빨간색 배경이 되도록함 - 버튼을 눌러서 색 변경된 후 포커스가 버튼으로 넘어감

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from "./Say";
-import EventPractice from "./EventPractice";
+// import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
 
-const App = () => {
-  return <EventPractice />;
-  // return <Say />;
-  // return <Counter />;
-  // return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
-};
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
+// const App = () => {
+// return <EventPractice />;
+// return <Say />;
+// return <Counter />;
+// return <MyComponent name="React" favoriteNumber={1}>리액트</MyComponent>;
+// };
 
 export default App;
