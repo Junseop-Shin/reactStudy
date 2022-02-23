39d94e634f75c03a9a98a9679c358c6a19ffa14f
junseop_shin <nuclearbomb6518@gmail.com>
2022-02-23 19:56:02 +0900

    [feat] 컴포넌트 반복 - 배열의 내장함수 map을 이용하면 데이터를 변경해 새로운 배열을 얻을 수 있음 - 배열의 내장함수 concat을 이용해 항목을 추가한 배열을 새로 만들어 setState해줘야 함 - 배열의 내장함수 filter를 이용해 항목을 제거한 배열을 새로 만들어 setState해줘야 함 - input을 이용해 버튼을 누르면 항목 추가, 항목 더블클릭 시 제거:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from "react";
-import ScrollBox from "./ScrollBox";
+// import ScrollBox from "./ScrollBox";
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from "./Say";
 // import EventPractice from "./EventPractice";
 // import ValidationSample from "./ValidationSample";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox
-          ref={(ref) => {
-            this.scrollBox = ref;
-          }}
-        />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 // const App = () => {
