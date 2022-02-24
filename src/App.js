c97d97746f2e6a949bd91c8119f04cc3b9f47f03
junseop_shin <nuclearbomb6518@gmail.com>
2022-02-24 19:32:57 +0900

    [feat] componentDidCatch 이용 error 잡기 - ErrorBoundary 컴포넌트에서 componentDidCatch를 이용해 error가 발생하면 children을 에러 발생 메시지로 바꿔줌 - App.js에서 LifeCycleSample 컴포넌트를 ErrorBoundary로 감싸줌으로써 LifeCycleSample에서 에러 발생 시 메시지로 바뀜

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
+import ErrorBoundary from "./ErrorBoundary";
 // import ScrollBox from "./ScrollBox";
 // import MyComponent from './MyComponent';
 // import Counter from './Counter';
 // import Say from "./Say";
 // import EventPractice from "./EventPractice";
 // import ValidationSample from "./ValidationSample";
-import IterationSample from "./IterationSample";
+// import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+
+// 랜덤 색상을 성성
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 // const App = () => {
