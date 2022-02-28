e39d8a8d405b32b5d2c14c684a3c237a582f3fc9
junseop_shin <nuclearbomb6518@gmail.com>
2022-02-28 19:13:10 +0900

    [feat] custom Hooks 사용

--- src/Info.js
+++ src/Info.js
@@ -1,20 +1,12 @@
 import React, { useReducer } from "react";
+import useInputs from "./useInputs";
 
-function reducer(state, action) {
-  return {
-    ...state,
-    [action.name]: action.value,
-  };
-}
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs(reducer, {
     name: "",
     nickname: "",
   });
   const { name, nickname } = state;
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
