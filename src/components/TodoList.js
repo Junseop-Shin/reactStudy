56e63038e0b5aa08a2173feb4538def6df33fabe
junseop_shin <nuclearbomb6518@gmail.com>
2022-07-09 15:21:01 +0900

    [feat] Todo 리스트 삭제 및 체크 토글 기능 추가 - App.js 에서 관리하는 todo array에서 id에 해당하는 item 제거 - 토글 시 id에 해당하는 item의 checked 속성을 변경

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
       {todos.map((todo) => (
-        <TodoListItem todo={todo} keyj={todo.id} />
+        <TodoListItem
+          todo={todo}
+          keyj={todo.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
