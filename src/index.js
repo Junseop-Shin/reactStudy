8cd385e94af1bd5c4c15a7a27a373550c78f3f07
junseop_shin <nuclearbomb6518@gmail.com>
2022-07-16 14:23:29 +0900

    [feat] Container 만들기 - connect를 이용해 Container의 props와 액션 생성 함수를 연결해 사용

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
