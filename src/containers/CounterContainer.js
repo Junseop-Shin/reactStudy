ec40d39169aa0747dc211c5c30bed1c2f9b47b17
junseop_shin <nuclearbomb6518@gmail.com>
2022-07-16 14:28:02 +0900

    [feat] bindActionCreators 이용 - dispatch를 하나하나 하지 않고 한번에 처리할 수 있음 - connect함수의 두 번째 파라미터로 객체를 넣어주면 connect 내부적으로 bindActionCreators를 수행함

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
@@ -9,17 +10,25 @@ const CounterContainer = ({ number, increase, decrease }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
-  number: state.counter.number,
-});
-const mapDispatchToProps = (dispatch) => ({
-  // 임시 함수
-  increase: () => {
-    dispatch(increase());
-  },
-  decrease: () => {
-    dispatch(decrease());
-  },
-});
+// const mapStateToProps = (state) => ({
+//   number: state.counter.number,
+// });
+// const mapDispatchToProps = (dispatch) => ({
+//   // 임시 함수
+//   increase: () => {
+//     dispatch(increase());
+//   },
+//   decrease: () => {
+//     dispatch(decrease());
+//   },
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => bindActionCreators({ increase, decrease }, dispatch),
+  //{ increase, decrease, }, 두 번째 파라미터를 객체 형태로 넣어주면 connect가 내부적으로 bindActionCreators 작업을 대신 해줌
+)(CounterContainer);
