bcb570934c56fcfb6199b00da97f09db0fad18c6
junseop_shin <nuclearbomb6518@gmail.com>
2022-07-17 15:04:03 +0900

    [refactor] redux 제공 hooks를 이용해 간단하게 코드정리 - useSeletor/useDispatch/useActions 등 사용 - connect 대신 useSelector를 사용하는 경우 React.memo를 사용해 최적화 필요

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { useCallback } from 'react';
+// import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+// import { bindActionCreators } from 'redux';
 import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
   );
 };
+
+export default CounterContainer;
+
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
 
 // const mapStateToProps = (state) => ({
 //   number: state.counter.number,
@@ -25,10 +38,10 @@ const CounterContainer = ({ number, increase, decrease }) => {
 
 // export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
 
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  (dispatch) => bindActionCreators({ increase, decrease }, dispatch),
-  //{ increase, decrease, }, 두 번째 파라미터를 객체 형태로 넣어주면 connect가 내부적으로 bindActionCreators 작업을 대신 해줌
-)(CounterContainer);
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   (dispatch) => bindActionCreators({ increase, decrease }, dispatch),
+//   //{ increase, decrease, }, 두 번째 파라미터를 객체 형태로 넣어주면 connect가 내부적으로 bindActionCreators 작업을 대신 해줌
+// )(CounterContainer);
