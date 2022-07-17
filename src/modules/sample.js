9efdb7b9567ad4bb095d076b5a4ba96c18906abc
junseop_shin <nuclearbomb6518@gmail.com>
2022-07-17 16:31:33 +0900

    [refactor] 기능 분리를 통한 코드 정리

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,5 +1,6 @@
 import { handleActions } from "redux-actions";
 import * as api from "../lib/api";
+import createRequestThunk from "../lib/createRequestThunk";
 
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
@@ -11,41 +12,8 @@ const GET_USER_FAILURE = "sample/GET_USER_FAILURE";
 
 // thunk
 
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST });
-  try {
-    const reponse = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: reponse.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e; // 나중에 컴포넌트 단에서 에러를 조회할 수 있게 해줌
-  }
-};
-
-export const getUsers = () => async (dispatch) => {
-  dispatch({ type: GET_USER });
-  try {
-    const reponse = await api.getUsers();
-    dispatch({
-      type: GET_USER_SUCCESS,
-      payload: reponse.data,
-    });
-  } catch (e) {
-    dispatch({
-      type: GET_USER_FAILURE,
-      payload: e,
-      error: true,
-    });
-    throw e;
-  }
-};
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USER, api.getUsers);
 
 const initialState = {
   loading: {
@@ -58,49 +26,13 @@ const initialState = {
 
 const sample = handleActions(
   {
-    [GET_POST]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: true,
-      },
-    }),
     [GET_POST_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
       post: action.payload,
-    }),
-    [GET_POST_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
-    }),
-    [GET_USER]: (state) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_USER: true,
-      },
     }),
     [GET_USER_SUCCESS]: (state, action) => ({
       ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
       users: action.payload,
-    }),
-    [GET_USER_FAILURE]: (state, action) => ({
-      ...state,
-      loading: {
-        ...state.loading,
-        GET_POST: false,
-      },
     }),
   },
   initialState
